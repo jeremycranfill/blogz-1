e7be2da8d63e10a218f2006f202085f6fb09b83d
Jeremy <jeremy.cranfill@gmail.com>
2017-02-16 01:09:03 -0600

    all done!

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ class BlogHandler(webapp2.RequestHandler):
         return query.fetch(limit=limit, offset=offset)
 
     def get_posts_by_user(self, user, limit, offset):
-        """
-            Get all posts by a specific user, ordered by creation date (descending).
-            The user parameter will be a User object.
-        """
+        user_id = user.key()
+        query = Post.all().order('-created')
+        query.filter('author =', user_id)
+        posts = query.fetch(offset=offset, limit=limit)
 
-        # TODO - filter the query so that only posts by the given user
-        return None
+
+        return posts
 
     def get_user_by_name(self, username):
         """ Get a user object from the db, based on their username """
@@ -258,7 +258,6 @@ class SignupHandler(BlogHandler):
 
 class LoginHandler(BlogHandler):
 
-    # TODO - The login code here is mostly set up for you, but there isn't a template to log in
 
     def render_login_form(self, error=""):
         """ Render the login form with or without an error, based on parameters """
